b798fcc8ac3b34806027cf1f703f23dfb3e97a04
XiaoJiang0208 <jjm1837080396@gmail.com>
2024-04-01 15:21:59 +0800

    update

--- server/ClientUpdaterServer.py
+++ server/ClientUpdaterServer.py
@@ -1,52 +1,57 @@
 import signal
 import sys
 from flask import *
+from flask_cors import CORS
 import time
 import os
 import toml
 import hashlib
 import threading
+import sqlite3
 
 
-SERVER = Flask(__name__)
 SERVER_PATH = "E:\ClientUpdater\server"
+
 
 def getFileMD5(path:str) -> str:
     with open(path,"rb") as f:
         bytes = f.read() # read file as bytes
         return hashlib.md5(bytes).hexdigest();
 
+def getFileDir():
+    return os.path.dirname(os.path.abspath(__file__)).replace("\\","/")
 
 #update
 class Update():
-    def __init__(self,path,uptime=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())) -> None:
-        self.update_time=uptime
+    def __init__(self,path) -> None:
+        self.update_time="0"
         self.dir=path
         self.update_logs="test"
-        self.mod_list=dict()
+        self.mods_list=dict()
 
     def makeModList(self):
-        self.mod_list=dict()
+        self.mods_list=dict()
         for f in os.listdir(self.dir+"/mods"):
-            self.mod_list[getFileMD5(self.dir+"/mods/"+f)]=f;
-        for f in os.listdir(self.dir+"/clientmods"):
-            self.mod_list[getFileMD5(self.dir+"/clientmods/"+f)]=f;
+            self.mods_list[getFileMD5(self.dir+"/mods/"+f)]=f;
+        if os.path.exists(self.dir+"/clientmods"):
+            for f in os.listdir(self.dir+"/clientmods"):
+                self.mods_list[getFileMD5(self.dir+"/clientmods/"+f)]=f;
 
     def makeJson(self) -> Response:
         return jsonify({"update_time":self.update_time,
                         "update_logs":self.update_logs,
-                        "mods_list":list(self.mod_list.keys())})
+                        "mods_list":list(self.mods_list.keys())})
 
     def getModPath(self,md5) -> str:
-        if os.path.exists(self.dir+"/mods/"+self.mod_list[md5]):
-            return self.dir+"/mods/"+self.mod_list[md5]
-        elif os.path.exists(self.dir+"/clientmods/"+self.mod_list[md5]):
-            return self.dir+"/clientmods/"+self.mod_list[md5]
+        if os.path.exists(self.dir+"/mods/"+self.mods_list[md5]):
+            return self.dir+"/mods/"+self.mods_list[md5]
+        elif os.path.exists(self.dir+"/clientmods/"+self.mods_list[md5]):
+            return self.dir+"/clientmods/"+self.mods_list[md5]
 
     def comment(self,msg,file=""):
         if file!="":
             try:
-                msg=open(os.path.dirname(os.path.abspath(__file__))+"/"+file,"r",encoding="utf-8")
+                msg=open(getFileDir()+"/"+file,"r",encoding="utf-8")
                 self.update_logs=msg.read()
                 print("已提交更新\n"+self.update_logs)
                 msg.close()
@@ -61,33 +66,97 @@ class Update():
 
 
 def readData():
-    if os.path.exists(os.path.dirname(os.path.abspath(__file__))+"/"+"data.csv"):
-        with open(os.path.dirname(os.path.abspath(__file__))+"/"+"data.csv","r",encoding="utf-8") as f:
-            d=f.read().split(",",2)
-            UPDATE.update_time=d[0]
-            UPDATE.update_logs=d[1]
-            UPDATE.mod_list=eval(d[2])
+    CUR.execute("""SELECT * FROM update_list
+        ORDER BY update_time DESC""")
+    data=CUR.fetchone()
+    if data:
+        UPDATE.update_time=data[0]
+        UPDATE.update_logs=data[1]
+        UPDATE.mods_list=eval(data[2])
     else:
         UPDATE.update_time=""
         UPDATE.update_logs=""
-        UPDATE.mod_list=dict()
+        UPDATE.mods_list=dict()
+    '''if os.path.exists(getFileDir()+"/"+"data.csv"):
+        with open(getFileDir()+"/"+"data.csv","r",encoding="utf-8") as f:
+            d=f.read().split(",",2)
+            UPDATE.update_time=d[0]
+            UPDATE.update_logs=d[1]
+            UPDATE.mods_list=eval(d[2])
+    else:
+        UPDATE.update_time=""
+        UPDATE.update_logs=""
+        UPDATE.mods_list=dict()'''
 
 def saveData():
-    with open(os.path.dirname(os.path.abspath(__file__))+"/"+"data.csv","w",encoding="utf-8") as f:
+    CUR.execute("INSERT INTO update_list VALUES (?,?,?)",(UPDATE.update_time,UPDATE.update_logs,str(UPDATE.mods_list)))
+    CONN.commit()
+    '''CUR.execute("SELECT * FROM update_list")
+    print(CUR.fetchall())
+    with open(getFileDir()+"/"+"data.csv","w",encoding="utf-8") as f:
         d=[]
         d.append(UPDATE.update_time)
         d.append(UPDATE.update_logs)
-        d.append(str(UPDATE.mod_list))
-        f.write(",".join(d))
+        d.append(str(UPDATE.mods_list))
+        f.write(",".join(d))'''
+
+def initDatabase():
+    try:
+        CUR.execute("""SELECT * FROM update_list;""")
+    except:
+        CUR.execute("""CREATE TABLE update_list
+            (update_time TEXT,
+            update_logs TEXT,
+            mods_list TEXT);""")
+        CONN.commit()
+        print("creat database")
+
+
+SERVER = Flask(__name__,
+            template_folder=getFileDir()+"/web",
+            static_folder=getFileDir()+"/web/assets")
+CORS(SERVER, resources=r'/*')
+
+CONN = sqlite3.connect(getFileDir()+"/data.db")
+CUR = CONN.cursor()
+
 
 #router
 @SERVER.route("/api/getupdate")
 def getUpdate():
     return UPDATE.makeJson()
 
+@SERVER.route("/api/getrangeupdate/<rg>")
+def getRangeUpdate(rg):
+    conn = sqlite3.connect(getFileDir()+"/"+"data.db")
+    cur = conn.cursor()
+    cur.execute("""SELECT COUNT(*) FROM update_list;""")
+    size=cur.fetchone()[0]
+    cur.execute("""SELECT * FROM update_list
+        ORDER BY update_time DESC""")
+    try:
+        rg=int(rg)
+    except:
+        rg=0
+    if rg < 0:
+        rg=0
+    if rg >= size:
+        rg=size-1
+    for _ in range(rg+1):
+        data=cur.fetchone()
+    update = Update(SERVER_PATH)
+    update.update_time=data[0]
+    update.update_logs=data[1]
+    update.mods_list=eval(data[2])
+    return update.makeJson()
+
 @SERVER.route("/api/download/<md5>")
 def download(md5):
     return send_file(UPDATE.getModPath(md5), as_attachment=True)
+
+@SERVER.route("/")
+def root():
+    return render_template("index.html")
 
 def runAPI(ip="0.0.0.0",port=25564):
     SERVER.run(ip,port)
@@ -96,6 +165,7 @@ def runAPI(ip="0.0.0.0",port=25564):
 #main
 UPDATE = Update(SERVER_PATH)
 if __name__ == "__main__":
+    initDatabase()
     readData()
     api = threading.Thread(target= runAPI, args=("0.0.0.0",25564))
     api.start()
@@ -117,7 +187,7 @@ if __name__ == "__main__":
             print("当前状态")
             print(UPDATE.update_time)
             print(UPDATE.update_logs)
-            for i in UPDATE.mod_list.items():
+            for i in UPDATE.mods_list.items():
                 print(i)
         else:
             print("unknow command")
